e024ac69030873161f3b371b8b6bcdad996566cb
Yuri <1009173009@QQ.COM>
2020-12-09 09:56:51 +0800

    添加项目文件。

---
+++ NET5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NET5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class InitDemo
+    {
+        public string Name { get; init; }
+        public int Age { get; init; }
+
+        public InitDemo() { }
+        public InitDemo(string name = "", int age = 0)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public void ChangeName(string name)
+        {
+            // 这里再编译时会直接报错
+            //this.Name = name;
+        }
+    }
+}
